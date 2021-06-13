9ab532dea70cc6ba7e5a3cd7a86c409775840294
Virginia <virginia.rabello2107@gmail.com>
2021-06-12 23:47:00 -0500

    route delete product running

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -39,6 +39,41 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes:[
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag,
+        as: 'tags_list'
+      }
+    ]
+  })
+  .then(dbProductData => {
+    if(!dbProductData){
+      res.status(404).json({ message: 'No product found with this id.'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
@@ -118,6 +153,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then( dbProductData => {
+    if(!dbProductData){
+      res.status(404).json({message: 'No product found with this id.'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
